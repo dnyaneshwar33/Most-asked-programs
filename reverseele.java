c4333cc212027f3798c9497629e2e4dae2228820
Dnyaneshwar Vighne <125205721+dnyaneshwar33@users.noreply.github.com>
2024-01-02 18:11:22 +0530

    Delete reverseele.java

--- reverseele.java
+++
@@ -1,9 +0,0 @@
-// Program to print the elements of an array in reverse order
-class reverseele{
-    public static void main(String args[]){
-        int nums[] ={12,34,56,78,89,98};
-        for(int i=nums.length-1;i>=0;i--){
-            System.out.print(nums[i]+" ");
-        }
-    }
-}
